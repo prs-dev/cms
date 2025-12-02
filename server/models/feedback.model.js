05e39aa69671621f26375289e8688f967d616977
prs-dev <pratyushsri.25@gmail.com>
2025-12-02 11:07:49 +0530

    added base routes, user, feedback, and admin

---
+++ server/models/feedback.model.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const feedbackSchema = new mongoose.Schema({
+
+})
+
+module.exports = mongoose.model("Feedback", feedbackSchema)
--- server/server.js
+++ server/server.js
@@ -6,6 +6,49 @@ app.get("/", (req, res) => {
     res.send("hello re")
 })
 
+//user
+//register
+app.post("/register", (req, res) => {
+    res.send("user register endpoint reached")
+})
+
+//login
+app.post("/login", (req, res) => {
+    res.send("user login endpoint reached")
+})
+
+//user feedback
+app.post('/feedback', (req, res) => {
+    res.send("user feedback endpoint reached")
+})
+
+//getting my feedback
+app.get('/feedback/me', (req, res) => {
+    res.send("my feedback endpoint reached")
+})
+
+//admin routes
+//get all feedback
+app.get('/feedback/all', (req, res) => {
+    res.send("admin feedback endpoint reached") 
+})
+
+//get one feedback
+app.get('/feedback/:id', (req, res) => {
+    res.send("admin feedback with id endpoint reached") 
+})
+
+//update status
+app.put("/feedback/update/:id/status", (req, res) => {
+    res.send("admin feedback update endpoint reached") 
+})
+
+//enter admin note of feedback
+app.post("/feedback/:id/note", (req, res) => {
+    res.send("admin feedback note endpoint reached") 
+})
+
+//fallback
 app.all('/*id', (req, res) => {
     res.send("not available")
 })
